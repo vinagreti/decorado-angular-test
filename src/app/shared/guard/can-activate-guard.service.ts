3a68d2322e5c463921d7068ae6045d9fb11f8725
Bruno João <bruno@tzadi.com>
2017-08-21 12:02:46 -0300

    Reactive guard service with singleton subscription

--- src/app/shared/guard/can-activate-guard.service.ts
+++ src/app/shared/guard/can-activate-guard.service.ts
@@ -2,20 +2,31 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 import { UserService } from './../../user/';
 
+/*
+* CURRENT SUBSCRIPTION
+*
+* Used to run the guard in a reactive way. Changes done after the first digest cicle will be catch by the subscription.
+* Singleton to avoid guards colision
+*/
+var currentSubscription = undefined;
+
 // Admin Level
 @Injectable()
 export class AdminGuardService implements CanActivate {
     constructor(private userService: UserService, private router: Router) { console.log('AdminGuardService STARTED') }
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        if(currentSubscription){
+            currentSubscription.unsubscribe();
+        }
         if (this.userService.isAdmin()) {
-            this.userService.user.subscribe((user) => {
+            currentSubscription = this.userService.user.subscribe((user) => {
                 if (!this.userService.isAdmin()) {
-                    this.router.navigate(['/']);
+                    console.log('NAVEGOU POR AQUI 1'); this.router.navigate(['/']);
                 }
             });
             return true;
         } else {
-            this.router.navigate(['/']);
+            console.log('NAVEGOU POR AQUI 2'); this.router.navigate(['/']);
             return false;
         }
     }
@@ -26,15 +37,18 @@ export class AdminGuardService implements CanActivate {
 export class LoggedGuardService implements CanActivate {
     constructor(private userService: UserService, private router: Router) { console.log('LoggedGuardService STARTED') }
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        if(currentSubscription){
+            currentSubscription.unsubscribe();
+        }
         if (this.userService.isLoggedIn()) {
-            this.userService.user.subscribe((user) => {
+            currentSubscription = this.userService.user.subscribe((user) => {
                 if (!this.userService.isLoggedIn()) {
-                    this.router.navigate(['/']);
+                    console.log('NAVEGOU POR AQUI 3'); this.router.navigate(['/']);
                 }
             });
             return true;
         } else {
-            this.router.navigate(['/']);
+            console.log('NAVEGOU POR AQUI 4'); this.router.navigate(['/']);
             return false;
         }
     }
@@ -45,15 +59,19 @@ export class LoggedGuardService implements CanActivate {
 export class PublicGuardService implements CanActivate {
     constructor(private userService: UserService, private router: Router) { console.log('PublicGuardService STARTED') }
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        if(currentSubscription){
+            currentSubscription.unsubscribe();
+        }
+
         if (!this.userService.isLoggedIn()) {
-            this.userService.user.subscribe((user) => {
+            currentSubscription = this.userService.user.subscribe((user) => {
                 if (this.userService.isLoggedIn()) {
-                    this.router.navigate(['/']);
+                    console.log('NAVEGOU POR AQUI 5'); this.router.navigate(['/']);
                 }
             });
             return true;
         } else {
-            this.router.navigate(['/']);
+            console.log('NAVEGOU POR AQUI 6'); this.router.navigate(['/']);
             return false;
         }
     }
